157fbab9a60db02c1fb0f8ddb36cef5045ecd5c2
root <whqkrqkr12345@gmail.com>
2025-02-18 11:45:00 +0900

    c_10709

--- c_10709/main.swift
+++ c_10709/main.swift
@@ -1,1 +1,26 @@
 import Foundation
+
+let n = readLine()!.split(separator: " ").map { Int($0)! }
+var board = [[String]]()
+var result = [[Int]](repeating: [Int](repeating: -1, count: n[1]), count: n[0])
+
+for _ in 0..<n[0] {
+    board.append(Array(readLine()!).map { String($0) })
+}
+
+for i in 0..<n[0] {
+    var m = -1
+    for j in 0..<n[1] {
+        if board[i][j] == "c" {
+            m = 0
+        }
+        if m != -1 {
+            result[i][j] = m
+            m += 1
+        }
+    }
+}
+
+for row in result {
+    print(row.map { String($0) }.joined(separator: " "))
+}
